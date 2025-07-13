c42f8bacc66b1a52681235c4e6d3ae3d588dbbec
Diego <diegodecstraid@gmail.com>
2025-07-13 03:21:24 -0300

    Resolvendo Desafio de Ordenacao
    Signed-off-by: Diego <diegodecstraid@gmail.com>

---
+++ src/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,43 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, int altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Juan", 10, 109);
+        ordenacaoPessoa.adicionarPessoa("Diego", 12, 108);
+        ordenacaoPessoa.adicionarPessoa("Brayon", 20, 107);
+        ordenacaoPessoa.adicionarPessoa("Lucas", 30, 106);
+
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+    }
+}
